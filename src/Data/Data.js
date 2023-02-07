48f075db963a74a7dcd013e9d7f1cd6578d562cf
mathesh <matheshyogeswaran@gmail.com>
2023-02-07 22:48:34 +0530

    Bar chart done

---
+++ src/Data/Data.js
@@ -0,0 +1,44 @@
+export const UserData = [
+  {
+    id: 1,
+    year: 2010,
+    userGain: 80000,
+    userLost: 823,
+  },
+  {
+    id: 2,
+    year: 2011,
+    userGain: 45677,
+    userLost: 345,
+  },
+  {
+    id: 3,
+    year: 2012,
+    userGain: 78888,
+    userLost: 555,
+  },
+  {
+    id: 4,
+    year: 2013,
+    userGain: 90000,
+    userLost: 4555,
+  },
+  {
+    id: 5,
+    year: 2014,
+    userGain: 4300,
+    userLost: 234,
+  },
+  {
+    id: 6,
+    year: 2015,
+    userGain: 90000,
+    userLost: 4555,
+  },
+  {
+    id: 7,
+    year: 2016,
+    userGain: 78888,
+    userLost: 555,
+  },
+];
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,36 @@
+import BarChart from "../components/BarChart";
+import { UserData } from "../Data/Data";
+import { useState } from "react";
+
 const Home = () => {
-    return (
-        <div>
-            <h1>this is home</h1>
-        </div>
-    )
-}
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: UserData.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+          "#2a71d0",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+
+  return (
+    <div>
+      <h1>Charts</h1>
+      <div style={{ width: 600 }}>
+        <BarChart chartData={userData} />
+      </div>
+    </div>
+  );
+};
 export default Home;
